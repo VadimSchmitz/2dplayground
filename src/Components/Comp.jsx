365ad8e983cdcef320e3a754a0917395c846cd82
Vadim S <di3mpi3@gmail.com>
2022-09-04 22:41:04 +0200

    PROGRESS

--- src/Components/Comp.jsx
+++ src/Components/Comp.jsx
@@ -1,71 +1,111 @@
-import { useEffect, useRef } from "react";
-import {
-  Engine,
-  Render,
-  Bodies,
-  World,
-  MatterMouse,
-  MatterMouseConstraint,
-} from "matter-js";
+import { useEffect } from "react";
+import Matter from "matter-js";
 
-function Comp(props) {
-  const scene = useRef();
-  const engine = useRef(Engine.create());
-
-  //test
-  let a = Bodies.rectangle(500, 100, 150, 50, { isStatic: false });
-  let b = Bodies.rectangle(600, 80, 150, 50, { isStatic: false });
+export default function App() {
+  let barA;
+  let barB;
 
   useEffect(() => {
-    const cw = document.body.clientWidth;
-    const ch = document.body.clientHeight;
+    var Engine = Matter.Engine,
+      Render = Matter.Render,
+      Events = Matter.Events,
+      MouseConstraint = Matter.MouseConstraint,
+      Mouse = Matter.Mouse,
+      World = Matter.World,
+      Bodies = Matter.Bodies;
 
-    const render = Render.create({
-      element: scene.current,
-      engine: engine.current,
+    // create an engine
+    var engine = Engine.create(),
+      world = engine.world;
+
+    // create a renderer
+    var render = Render.create({
+      element: document.body,
+      engine: engine,
       options: {
-        width: cw,
-        height: ch,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        pixelRatio: 2,
         wireframes: false,
-        background: "transparent",
       },
     });
 
-    //borders
-    World.add(engine.current.world, [
-      Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true }),
-      Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true }),
-      Bodies.rectangle(cw / 2, ch + 10, cw, 20, { isStatic: true }),
-      Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true }),
-      a,
-      b,
-    ]);
+    const cw = document.body.clientWidth;
+    const ch = document.body.clientHeight;
 
-    Engine.run(engine.current);
+    let ground = Bodies.rectangle(cw / 2, -10, cw, 20, { isStatic: true });
+    let leftWall = Bodies.rectangle(-10, ch / 2, 20, ch, { isStatic: true });
+    let rightWall = Bodies.rectangle(cw / 2, ch + 10, cw, 20, {
+      isStatic: true,
+    });
+    let ceiling = Bodies.rectangle(cw + 10, ch / 2, 20, ch, { isStatic: true });
+
+    // object colors & variables
+
+    // create objects
+    let a = Bodies.rectangle(500, 100, 150, 50, {
+      isStatic: false,
+      inertia: Infinity,
+    });
+    let b = Bodies.rectangle(600, 80, 150, 50, { isStatic: false });
+
+    barA = a;
+    barB = b;
+
+    // add all of the bodies to the world
+    World.add(engine.world, [ground, leftWall, rightWall, ceiling, a, b]);
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.1,
+          render: {
+            visible: false,
+          },
+        },
+      });
+
+    const runner = Matter.Runner.create();
+    Matter.Events.on(runner, "tick", (event) => {
+      if (mouseConstraint.body) {
+        console.log(
+          mouseConstraint.body.vertices[0],
+          mouseConstraint.body.vertices[1],
+          mouseConstraint.body.vertices[2],
+          mouseConstraint.body.vertices[3]
+        );
+        console.log(
+          mouseConstraint.mouse.absolute.x,
+          mouseConstraint.mouse.absolute.y
+        );
+      }
+    });
+
+    World.add(world, mouseConstraint);
+
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+    // run the engine
+    Matter.Runner.start(runner, engine);
+    // run the renderer
     Render.run(render);
-
-    return () => {
-      Render.stop(render);
-      World.clear(engine.current.world);
-      Engine.clear(engine.current);
-      render.canvas = null;
-      render.context = null;
-      render.textures = {};
-    };
-  }, []);
+  });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log(b.position.x, b.position.y, b.angle);
+      // console.log(barB.position.x, barB.position.y, barB.angle);
+      // console.log(
+      //   barB.vertices[0],
+      //   barB.vertices[1],
+      //   barB.vertices[2],
+      //   barB.vertices[3]
+      // );
+      // barB.torque += 10;
     }, 10);
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <div>
-      <div ref={scene} style={{ width: "100%", height: "100%" }} />
-    </div>
-  );
+  return <div></div>;
 }
-
-export default Comp;
